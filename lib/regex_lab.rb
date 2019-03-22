66054ee7a86fc80482aed8f12335078c46d4ba2b
Michael Mottola <mottola64@gmail.com>
2019-03-22 01:12:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,11 +3,12 @@ return word.scan(/\b[aeiouAEIOU]\w*).length > 0
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+word_array = text.scan(/|bun\w*ing\b/)
+return word_array
 end
 
 def words_five_letters_long(text)
-
+word_array = text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
